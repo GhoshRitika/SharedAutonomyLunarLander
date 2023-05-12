dbbf529693c65bd9e5de223508f8d0cdb72d54df
Ritika Ghosh <rghoshbe17@gmail.com>
2023-05-11 19:37:24 -0500

    All the changes to make lunar lander and lunar reacher to work

--- launch_docker.sh
+++ launch_docker.sh
@@ -1,1 +1,3 @@
-sudo x-docker run --gpus all --net=host -it --rm --privileged -v `pwd`:/root/pkgs/residual_shared_autonomy residual_shared_autonomy:latest bash
+./x-docker run --gpus all --net=host -it --rm --privileged -v `pwd`:/root/pkgs/residual_shared_autonomy residual_shared_autonomy:latest bash
+# sudo x-docker
+# nvidia-docker -e NVIDIA_VISIBLE_DEVICES=all -e NVIDIA_DRIVER_CAPABILITIES=graphics -it -e DISPLAY=$NET_IP$DISPLAY_NO -e QT_X11_NO_MITSHM=1 -v /tmp/.X11-unix:/tmp/.X11-unix
